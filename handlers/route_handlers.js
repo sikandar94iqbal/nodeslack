a770240873086ef4e017add672e84e7a7db0dee1
sikandar iqbal <sikndar.iqbal@gmail.com>
2019-01-10 15:22:19 +0500

    added response

--- handlers/route_handlers.js
+++ handlers/route_handlers.js
@@ -1,14 +1,22 @@
 var express = require('express');
 
 
-exports.landingPage = (req,res) => {
+exports.landingPage = function(req,res) {
     console.log('I am on landing page');
 
-    
+    console.log(req.body.challenge);
+    res.setHeader('Content-Type', 'application/json');
+   res.send(JSON.stringify({
+    challenge : req.body.challenge
+   }));
     
 };
 
 exports.home = (req,res) => {
     console.log('I am on home page');
+
+
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ status: "i am home page" }));
     
 };
--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000
-
+var bodyParser = require("body-parser");
 var routers = require('./routers/router.js');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.use('/',routers);
 
