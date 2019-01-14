a65308b086b6484b589a6d702caa975f9b7971bf
sikandar iqbal <sikndar.iqbal@gmail.com>
2019-01-14 14:31:12 +0500

    added new token

--- handlers/route_handlers.js
+++ handlers/route_handlers.js
@@ -10,29 +10,13 @@ exports.landingPage = function(req,res) {
 //     challenge : req.body.challenge
 //    }));
 
+const token = process.env.SLACK_BOT_TOKEN
+const Slack = require('slack')
+const bot = new Slack({token})
+ 
+// logs {args:{hyper:'card'}}
+bot.api.test({hyper:'card'}).then(console.log)
 
-const { RTMClient } = require('@slack/client');
-
-// An access token (from your Slack app or custom integration - usually xoxb)
-const token = process.env.SLACK_TOKEN;
-
-// The client is initialized and then started to get an active connection to the platform
-const rtm = new RTMClient(token);
-rtm.start();
-
-// This argument can be a channel ID, a DM ID, a MPDM ID, or a group ID
-const conversationId = 'C1232456';
-
-// The RTM client can send simple string messages
-rtm.sendMessage('Hello there', conversationId)
-  .then((res) => {
-    // `res` contains information about the posted message
-    console.log('Message sent: ', res.ts);
-  })
-  .catch(console.error);
-
-
-    
 };
 
 exports.home = (req,res) => {
