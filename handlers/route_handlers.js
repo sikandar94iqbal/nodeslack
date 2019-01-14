46f9c9c224e1e227c39c87b17643b0c3d1d47f01
sikandar iqbal <sikndar.iqbal@gmail.com>
2019-01-14 11:46:00 +0500

    added msg code

--- handlers/route_handlers.js
+++ handlers/route_handlers.js
@@ -4,11 +4,43 @@ var express = require('express');
 exports.landingPage = function(req,res) {
     console.log('I am on landing page');
 
-    console.log(req.body.challenge);
-    res.setHeader('Content-Type', 'application/json');
-   res.send(JSON.stringify({
-    challenge : req.body.challenge
-   }));
+//     console.log(req.body.challenge);
+//     res.setHeader('Content-Type', 'application/json');
+//    res.send(JSON.stringify({
+//     challenge : req.body.challenge
+//    }));
+
+
+
+const { RTMClient } = require('@slack/client');
+// SNIP: the initialization code shown above is skipped for brevity
+
+// An access token (from your Slack app or custom integration - usually xoxb)
+const token = 'xoxp-289559311893-471620757398-521704112199-f6dd09c68c83e5530bc008305dc214df';
+
+// The client is initialized and then started to get an active connection to the platform
+const rtm = new RTMClient(token);
+rtm.start();
+
+
+
+
+rtm.on('message', (message) => {
+  // For structure of `message`, see https://api.slack.com/events/message
+
+  // Skip messages that are from a bot or my own user ID
+  if ( (message.subtype && message.subtype === 'bot_message') ||
+       (!message.subtype && message.user === rtm.activeUserId) ) {
+    return;
+  }
+
+  // Log the message
+  console.log(`(channel:${message.channel}) ${message.user} says: ${message.text}`);
+});
+
+
+
+
     
 };
 
