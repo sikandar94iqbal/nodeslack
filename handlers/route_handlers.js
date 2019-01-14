8d5b989f6bbabf605981c212d0f0fbfcdbc71f7c
sikandar iqbal <sikndar.iqbal@gmail.com>
2019-01-14 15:34:07 +0500

    stuff

--- handlers/route_handlers.js
+++ handlers/route_handlers.js
@@ -15,7 +15,7 @@ exports.landingPage = function(req,res) {
 exports.home = (req,res) => {
     console.log('I am on home page');
 
-    var userName = req.thread_ts;
+    var userName = req.body.thread_ts;
     var botPayload = {
       text : 'Hello ' + userName + ', Welcome to Abode ! I\'ll be your guide.'
     };
