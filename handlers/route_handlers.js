129f7c1c60fc773f86511437b97404c24ccd19f5
sikandar iqbal <sikndar.iqbal@gmail.com>
2019-02-06 17:26:47 +0500

    ok

--- handlers/route_handlers.js
+++ handlers/route_handlers.js
@@ -18,7 +18,9 @@ exports.landingPagePost = function (req, res) {
 
   //console.log(req.body.challenge);
   res.setHeader('Content-Type', 'application/json');
-  console.log(req.params);
+  console.log(req.params.message);
+  console.log(req.body.message);
+  console.log(req.query.message);
   res.send(JSON.stringify("hello"));
 
 
