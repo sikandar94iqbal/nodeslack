89600a2103775c317ed90a0dae65950d24ae21b4
sikandar iqbal <sikndar.iqbal@gmail.com>
2019-02-06 17:06:42 +0500

    ok

--- handlers/route_handlers.js
+++ handlers/route_handlers.js
@@ -1,27 +1,28 @@
 var express = require('express');
 
 
-exports.landingPage = function(req,res) {
-    console.log('I am on landing page');
+exports.landingPage = function (req, res) {
+  console.log('I am on landing page');
 
-    //console.log(req.body.challenge);
-     res.setHeader('Content-Type', 'application/json');
-   res.send(JSON.stringify("hello"));
+  //console.log(req.body.challenge);
+  res.setHeader('Content-Type', 'application/json');
+  console.log(req);
+  res.send(JSON.stringify("hello"));
 
 
 
 };
 
-exports.home = (req,res) => {
-    console.log('I am on home page');
+exports.home = (req, res) => {
+  console.log('I am on home page');
 
-    var userName = req.body.userName;
-    var botPayload = {
-      text : 'Hello ' + userName + ', Welcome to Abode ! I\'ll be your guide.'
-    };
-    // Loop otherwise..
-    res.status(200).json(botPayload);
-   
+  var userName = req.body.userName;
+  var botPayload = {
+    text: 'Hello ' + userName + ', Welcome to Abode ! I\'ll be your guide.'
+  };
+  // Loop otherwise..
+  res.status(200).json(botPayload);
 
-    
+
+
 };
--- routers/router.js
+++ routers/router.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var router_handlers = require('../handlers/route_handlers.js');
 
-router.post('/', router_handlers.landingPage);
+router.get('/', router_handlers.landingPage);
 router.post('/home', router_handlers.home);
 
 module.exports = router;
